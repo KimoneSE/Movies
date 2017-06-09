44e8d5b34723847980fb3251d55bf7f2fede94e0
KimoneSE <1799215066@qq.com>
2017-06-09 17:38:56 +0800

    entity+tab

--- WebContent/views/index.jsp
+++ WebContent/views/index.jsp
@@ -5,7 +5,23 @@
 	<link rel="stylesheet" href="../assets/css/bootstrap.css">
 </head>
 
-<body>
+<body style="margin-top:10px">
+	<div class="col-xs-offset-2 col-xs-8" id="myContent" style="min-height: 40px">
+		<ul class="nav nav-tabs" id="selectTab" style="font-family: 等线">
+           	<li id="release" class="active"><a href="#releasing" data-toggle="tab">正在热映</a></li>
+      	    <li id="all"><a href="#allMovies" data-toggle="tab">全部影片</a></li>
+        </ul>
+        <div id="myTabContent" class="tab-content">
+				<div class="tab-pane fade in active" id="releasing">
+					
+					
+				</div>
+				<div class="tab-pane fade" id="allMovies">
+					
+					
+				</div>
+			</div>
+		</div>
 	<script src="${pageContext.request.contextPath }/assets/js/jquery-2.1.4.min.js"></script>
 	<script src="${pageContext.request.contextPath }/assets/js/bootstrap.min.js"></script>
 	<script src="${pageContext.request.contextPath }/assets/js/bootstrap.js"></script>
---
+++ src/com/movie/model/ReleaseMovie.java
@@ -0,0 +1,91 @@
+package com.movie.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="release_movie")
+public class ReleaseMovie {
+	private String city;
+	private String movieName;
+	private String director;
+	private String duration;
+	private String img;
+	private String region;
+	private String releaseTime;
+	private String score;
+	private String star;
+	
+	@Column(name="city")
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	@Column(name="movieName")
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	
+	@Column(name="director")
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	
+	@Column(name="duration")
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	
+	@Column(name="img")
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	
+	@Column(name="region")
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	
+	@Column(name="releaseTime")
+	public String getReleaseTime() {
+		return releaseTime;
+	}
+	public void setReleaseTime(String releaseTime) {
+		this.releaseTime = releaseTime;
+	}
+	
+	@Column(name="score")
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+	
+	@Column(name="star")
+	public String getStar() {
+		return star;
+	}
+	public void setStar(String star) {
+		this.star = star;
+	}
+}
